7ce31e1926b7f9f8df5df819f306387ef872f4e9
J-ZhengLi <lizhengghengj@gmail.com>
2024-05-26 23:49:04 +0800

    tried to implement key mappings.
    a bit complicated than I'd imagine.

--- midi_keyboard_as_keyboard/main.py
+++ midi_keyboard_as_keyboard/main.py
@@ -1,45 +1,64 @@
 #!/usr/bin/env python3
 
+import time
 from os import path
+from enum import Enum
+
 import pickle
 
 import mido
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Controller, HotKey, Listener, GlobalHotKeys
+
+class State(Enum):
+    PLAYING = 1
+    MAPPING = 2
+    IDLE = 3
 
 keyboard = Controller()
+global_state = State.IDLE
 
 def main():
     ports = mido.get_input_names()
 
-    print("Available MIDI devices: ")
-    for i, port in enumerate(ports):
-        print(f"  {i}: {port}")
-
-    sel = input("Select a device: ")
+    if not ports:
+        print("waiting for MIDI device...")
+        while not ports:
+            time.sleep(1)
+            ports = mido.get_input_names()
+    
     try:
-        idx = 0 if not sel else int(sel)
-        device_name = ports[idx]
-        key_map = get_key_map()
+        index = 0
+        if len(ports) > 1:
+            print("Available MIDI devices: ")
+            for i, port in enumerate(ports):
+                print(f"  {i}: {port}")
+            index = int(input("Select a device: "))
+
+        # Already checked that `ports` is not empty, so this should be fine
+        device_name = ports[index]
+
         with mido.open_input(device_name) as port:
-            print(f"successfully connected to '{port}'")
+            print("successfully connected to '{}'".format(port.name))
+
+            key_map = get_key_map(port)
+
+            update_state(State.PLAYING)
             for msg in port:
-                # This is the left most key on my midi keyboard, allow it to act as `esc` key for now,
-                # otherwise there is no way but to close the whole shell to stop the program
-                # as `ctrl+c/z` are not working lol.
-                if msg.note == 21:
-                    break
                 handle_note(key_map, msg.note, msg.type == "note_on")
     except IndexError as ie:
         raise ie
     except ValueError as ve:
-        print(f"invalid choice of device '{sel}': {ve}")
+        print(f"invalid choice of device '{index}': {ve}")
         exit(1)
+    except KeyboardInterrupt:
+        # FIXME: rather than requiring another key pressing, find a way to exit immediately
+        exit(0)
     except Exception as uncaught:
         print("yo what's going on?", uncaught)
         exit(1)
 
 
-def get_key_map() -> dict:
+def get_key_map(port) -> dict:
     """ Try to read key map file, if it doesn't exist, start key mapping wizard. """
 
     script_dir = path.dirname(path.realpath(__file__))
@@ -60,12 +79,8 @@ def get_key_map() -> dict:
                 print("cannot run without key configuration, aborting...")
                 exit(1)
             elif ans.startswith("y"):
-                # TODO (running out of time, need to sleep, gotta work tomorrow... sad): replace this with the actual wizard
-                key_map = {
-                    69: Key.space,
-                    70: Key.backspace,
-                    71: "a",
-                }
+                key_map = key_remap(port)
+
                 with open(key_map_filepath, "wb") as key_map_file:
                     pickle.dump(key_map, key_map_file)
                 return key_map
@@ -77,6 +92,45 @@ def get_key_map() -> dict:
         exit(1)
     except Exception as ex:
         raise ex
+
+
+def update_state(new_state: State):
+    global global_state
+    global_state = new_state
+
+
+def key_remap(port) -> dict:
+    def on_save():
+        update_state(State.IDLE)
+    
+    def on_abort():
+        key_map = dict()
+        update_state(State.IDLE)
+
+    update_state(State.MAPPING)
+
+    key_map = dict()
+    
+    # Register hotkeys
+    # with GlobalHotKeys({"<ctrl>+s": on_save, "<ctrl>+q": on_abort}) as hotkeys:
+    #     hotkeys.join()
+    print(
+        "follow the instructions to remap keys,\n\
+        press '<ctrl> + s' to save,\n\
+        press '<ctrl> + q' to abort.\n"
+    )
+
+    while global_state is State.MAPPING:
+        print("press a key on MIDI device:", end=" ")
+
+        recv = port.receive()
+        print("note({})".format(recv.note))
+
+        # TODO: Handle keyboard event.
+        print("now press a key on keyboard:", end="")
+        update_state(State.IDLE)
+
+    return key_map
 
 
 def handle_note(key_map: dict, note, pressed=True):
